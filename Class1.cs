0752c5f975c711efb1ab15fd6ceb8c4a6a1858dc
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-02-11 16:51:55 -0500

    Papa Calc, take a load off.

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PapaCalc
+{
+    public class CalculatorStarter
+    {
+        public static void StartCalculator()
+        {
+            Process.Start("calc.exe");
+        }
+    }
+}
